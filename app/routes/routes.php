4d46f970dc08115f56a0265d8303b9294b1c9982
Davidfhk <davidign1688@hotmail.com>
2017-06-01 19:02:25 +0200

    Añadimos la ruta  app->get, para linkear al formulario donde añadiremos la nueva nota y app->post, para insertar una nueva nota

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -6,10 +6,37 @@ $app->get('/', function() use ($app){
 	$app->render('inicio.php');
 });
 
+//-----------------------Consulta para mostrar todas las notas--------------------
+
 $app->get('/notas', function() use ($app,$db){
 	$sql = $db->prepare("SELECT * FROM notas");
 	$sql->execute();
 	$data = $sql->fetchAll(PDO::FETCH_ASSOC);
 	
 	$app->render('notas.php',array('notas' => $data));
+});
+
+// ----------Consulta para linkear al archivo nuevo.php, donde crearemos un nuevo usuario-----------
+
+$app->get('/nueva/nota', function() use ($app){
+	$app->render('nuevo.php');
+});
+
+// ----------Consulta para crear el nuevo usuario--------------------
+
+$app->post('/nueva/nota', function() use ($app,$db){
+	$request = $app->request;
+	$nombre = $request->post('nombre');
+	$descripcion = $request->post('descripcion');
+
+	$sql = $db->prepare("INSERT INTO notas(nombre,descripcion) VALUES (:nombre,:descripcion)");
+	$guardado = $sql->execute(array(':nombre' => $nombre, ':descripcion' => $descripcion));
+
+	if ($guardado) {
+		$app->flash('message','Nota guardada con exito');
+	}else{
+		$app->flash('error','Fallo al guardar la nota');
+	}
+
+	$app->redirect('nota');
 });
