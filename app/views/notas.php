4a7338a58caf25a69100208101e702a4823eccd3
Davidfhk <davidign1688@hotmail.com>
2017-06-01 11:54:04 +0200

    Aqui mostraremos todas las notas

---
+++ app/views/notas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Notas</h1>
+	<table>
+	<?php foreach($notas as $nota):?>
+	
+		<tr>
+			<td><?= $nota['nombre']?></td>
+			<td><a href="">Editar</a></td>
+			<td><a href="">Borrar</a></td>
+			<td><a href="">Detalles</a></td>
+		</tr>
+	<?php endforeach; ?>
+		<tr>
+			<td colspan="3"><a href="">Añadir nueva nota</a></td>
+		</tr>
+		<tr>
+			<td colspan="3"><a href="">Inicio</a></td>
+		</tr>
+	</table>
+</body>
+</html>
