35f5aacb6d75ae7eddd09a2a869ee3367ee341a4
Davidfhk <davidign1688@hotmail.com>
2017-06-01 11:52:25 +0200

    Esta sera nuestra vista principal

---
+++ app/views/inicio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<h3>Para ver todas las notas</h3>
+	<a href="notas">Click aqui </a>
+<h3>Para ver tus notas favoritos</h3>
+	<a href="favoritas">Click aqui </a>
+</body>
+</html>
