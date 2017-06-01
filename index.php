5f1a698ea8fe5dbfcc3d5e3f083afcfd3284e6b0
Davidfhk <davidign1688@hotmail.com>
2017-06-01 11:51:11 +0200

    Este sera nuestro index

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'app/db/dbconnect.php';
+// aplicación
+$app = new \Slim\Slim();
+$db = Conexion::Connect();
+
+$app->config(array(
+			'debug' => true,
+			'templates.path' => 'app/views',
+			));
+
+
+require 'app/routes/routes.php';
+
+$app->run();
