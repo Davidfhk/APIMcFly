e8e3c8c75139a4551ccdd5e14a8bba947227f75f
David <davidign1688@hotmail.com>
2017-06-07 14:54:51 +0200

    Update inicio.php

--- app/views/inicio.php
+++ app/views/inicio.php
@@ -7,7 +7,7 @@
 <body>
 <h3>Para ver todas las notas</h3>
 	<a href="notas">Click aqui </a>
-<h3>Para ver tus notas favoritos</h3>
+<h3>Para ver tus notas favoritas</h3>
 	<a href="favoritas">Click aqui </a>
 </body>
 </html>
